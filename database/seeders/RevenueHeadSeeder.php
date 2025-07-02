bb3d2a3ffd2cadd60cbf4ad1f5cc6d10f1e32bd8
abdulquadri <awonugagbemileke089@gmail.com>
2025-07-02 06:50:28 +0100

    fix

--- database/seeders/RevenueHeadSeeder.php
+++ database/seeders/RevenueHeadSeeder.php
@@ -15,7 +15,7 @@ class RevenueHeadSeeder extends Seeder
    public function run()
 	{
 		$payment_revenue_heads = array(
-			array('id' => '1', 'revenue_head_name' => 'MOTOKA PAYMENT', 'revenue_head_code' => '200050072112001001', 'bank_id' => '1', 'gateway_id' => '1', 'fee_bearer' => 'merchant', 'status' => 'active', 'created_at' => '2025-06-28 16:49:13', 'updated_at' => '2025-06-28 16:49:13'),
+			array('id' => '1', 'revenue_head_name' => 'MOTOKA PAYMENT', 'revenue_head_code' => 'REV686003f87e350', 'bank_id' => '1', 'gateway_id' => '1', 'fee_bearer' => 'merchant', 'status' => 'active', 'created_at' => '2025-06-28 16:49:13', 'updated_at' => '2025-06-28 16:49:13'),
 		);
 
 		DB::table('revenue_heads')->insert($payment_revenue_heads);
